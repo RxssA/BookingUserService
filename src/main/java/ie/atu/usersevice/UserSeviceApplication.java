58c4d6df5f48d5a6dfc834981d25d64f788f47db
rossa <g00401655@atu.ie>
2024-11-09 14:57:16 +0000

    FirstPush

---
+++ src/main/java/ie/atu/usersevice/UserSeviceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.usersevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/usersevice/UserSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.usersevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
