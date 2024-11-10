0e7f82de6f34440503662ea459271a6955996df9
rossa <g00401655@atu.ie>
2024-11-10 13:49:15 +0000

    cant access h2 db

--- src/main/java/ie/atu/usersevice/TokenFilter.java
+++
@@ -1,9 +0,0 @@
-package ie.atu.usersevice;
-
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.WebFilter;
-
-
-@WebFilter("/api/*")
-public class TokenFilter extends OncePerRequestFilter {
-}
